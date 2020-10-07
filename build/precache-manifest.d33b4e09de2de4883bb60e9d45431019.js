5cb18272f6de7e2c612c0fd1b204d7d26a7ccb3b
bikrant bidari <64511430+bikrantbdr@users.noreply.github.com>
2020-10-07 12:17:24 +0545

    Add files via upload

---
+++ build/precache-manifest.d33b4e09de2de4883bb60e9d45431019.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6400a9511de85819afc9be6a2ae9bf8b",
+    "url": "/monsters-robolex/index.html"
+  },
+  {
+    "revision": "84c1b6ecf9d83cd8ff9b",
+    "url": "/monsters-robolex/static/css/main.29ab5c9e.chunk.css"
+  },
+  {
+    "revision": "6952c745d55c5a0fa7de",
+    "url": "/monsters-robolex/static/js/2.e47ca8d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-robolex/static/js/2.e47ca8d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84c1b6ecf9d83cd8ff9b",
+    "url": "/monsters-robolex/static/js/main.d64f53be.chunk.js"
+  },
+  {
+    "revision": "f8b373947f91c0269f8b",
+    "url": "/monsters-robolex/static/js/runtime-main.c655e2ee.js"
+  }
+]);
